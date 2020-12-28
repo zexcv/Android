0fbb994555b57eddc9ca4a276e1ff1f6f7d56118
zexcv <krusher13@o2.pl>
2020-12-28 15:21:40 +0100

    Second layout, difficulty button

--- app/src/main/java/com/example/aplikacjazaliczenie/MainActivity.java
+++ app/src/main/java/com/example/aplikacjazaliczenie/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.aplikacjazaliczenie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.difficulty);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, difficulty.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 
 
---
+++ app/src/main/java/com/example/aplikacjazaliczenie/difficulty.java
@@ -0,0 +1,14 @@
+package com.example.aplikacjazaliczenie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class difficulty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_difficulty);
+    }
+}
